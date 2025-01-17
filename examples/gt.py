1fc3a278dccd7a17d07f15e2288e5cf91333bd4e
csrpas <adhominem17@gmail.com>
2025-01-17 16:33:00 +0100

    Update gt.py

--- examples/gt.py
+++ examples/gt.py
@@ -9,6 +9,5 @@ with SB(uc=True, test=True, locale_code="en") as sb:
     sb.sleep(2)
     sb.uc_gui_click_captcha()
     sb.sleep(2)
-    print("FOUND", 
-        sb.get_text("//*[@id='MasterGC_ContentBlockHolder_lblNombreProv']")
-         )
+    print(sb.get_text("//*[@id='MasterGC_ContentBlockHolder_lblNombreProv']"))
+    
